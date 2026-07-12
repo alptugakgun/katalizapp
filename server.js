a1b012ddb9b24ae381761261200053fbc215beca
alptugakgun <74013971+alptugakgun@users.noreply.github.com>
2026-07-12 18:35:06 +0300

    fix: updated CSP for CDNs and media, updated actual dashboard screenshots

--- server.js
+++ server.js
@@ -17,11 +17,12 @@ app.use(helmet({
     contentSecurityPolicy: {
         directives: {
             defaultSrc: ["'self'"],
-            scriptSrc: ["'self'", "'unsafe-inline'", "https://cdn.socket.io"],
+            scriptSrc: ["'self'", "'unsafe-inline'", "https://cdn.socket.io", "https://cdn.jsdelivr.net", "https://cdnjs.cloudflare.com"],
             scriptSrcAttr: ["'unsafe-inline'"],
-            styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+            styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://cdnjs.cloudflare.com"],
             fontSrc: ["'self'", "https://fonts.gstatic.com"],
             imgSrc: ["'self'", "data:", "https://cdn-icons-png.flaticon.com", "https://*"],
+            mediaSrc: ["'self'", "https://assets.mixkit.co"],
             connectSrc: ["'self'", "wss:", "ws:"]
         }
     }
